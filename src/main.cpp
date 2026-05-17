a78e520212b2b41ae687721a268969fa3d83ce51
Gaba5454 <egor.shvarev@bk.ru>
2026-05-17 18:43:39 +0700

    Added linear interpolation

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,29 @@
 #include <cmath>
 #include "../include/sine_generator.h"
 #include "../include/quantization.h"
+#include "../include/interpolation.h"
+
 int main() {
 
-    double f = 10;
-    double fs = 50;
+    double f = 33;
+    double fs = 100;
     int num_samples = 50;
 
     std::vector<double> signal = sine_generator(f,fs,num_samples);
+    for(size_t i = 0; i < 5; ++i){
+        std::cout << signal[i] << " ";
+    }
     std::vector<int16_t> quant_sig = quantization(signal);
     double error = quantization_error(signal, quant_sig);
-    std::cout << error;
+
+    std::cout << "\n" << error << "\n";
+    
+    std::vector<double> inter_sig = linear_interpolation(signal);
+
+    for(size_t i = 0; i < 10; ++i){
+        std::cout << inter_sig[i] << " ";
+    }
+    std::cout << "\n";
     return 0;
 
 }
