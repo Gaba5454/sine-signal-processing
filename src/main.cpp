19dc9d976ccb092df78390dc1cd7d0be572ffdf9
Gaba5454 <egor.shvarev@bk.ru>
2026-05-17 14:50:58 +0700

    Added a function for calculating quantization error

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 #include <vector>
 #include <cmath>
 #include "../include/sine_generator.h"
-
+#include "../include/quantization.h"
 int main() {
 
     double f = 10;
     double fs = 50;
     int num_samples = 50;
-    std::vector<double> signal = sine_generator(f,fs,num_samples);
 
-    for (size_t i = 0; i < signal.size(); i++){
-        std::cout << signal[i] << "\n";
-    }
+    std::vector<double> signal = sine_generator(f,fs,num_samples);
+    std::vector<int16_t> quant_sig = quantization(signal);
+    double error = quantization_error(signal, quant_sig);
+    std::cout << error;
     return 0;
 
 }
