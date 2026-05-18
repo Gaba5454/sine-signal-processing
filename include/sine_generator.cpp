fd0b01f90e291833488915f85904ba96ca24cf4f
Gaba5454 <egor.shvarev@bk.ru>
2026-05-18 11:00:06 +0700

    Delete comment

--- include/sine_generator.cpp
+++ include/sine_generator.cpp
@@ -1,15 +1,5 @@
 #include "sine_generator.h"
 
-/**
- * Генератор синусоидального сигнала
- * 
- * @param f_hz Частота сигнала (от 0 до 50 Гц)
- * @param fs_hz Частота дискретизации (фиксирована 100 Гц)
- * @param num_samples Количество отсчетов (длина сигнала)
- * @return std::vector<double> Массив сгенерированных значений
- *
- */
-
 std::vector<double> sine_generator(double f_hz, double fs_hz, int num_samples) {
     std::vector<double> signal;
     signal.reserve(num_samples); 
