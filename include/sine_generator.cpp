8b2622f06b175777a4c5a17cfc9e650af9a6f489
Gaba5454 <egor.shvarev@bk.ru>
2026-05-18 16:23:11 +0700

    Quick fix

--- include/sine_generator.cpp
+++ include/sine_generator.cpp
@@ -4,13 +4,12 @@ std::vector<double> sine_generator(double f_hz, double fs_hz, int num_samples) {
     std::vector<double> signal;
     signal.reserve(num_samples); 
 
-    // Защита от некорректных данных
     if (fs_hz <= 0) {
         throw std::invalid_argument("fs_hz must be positive");
     }
     
     for (int n = 0; n < num_samples; ++n) {
-        // Формула дискретного синуса:
+
         // x[n] = sin(2 * pi * f * n / Fs)
         // где n/Fs - это время t в секундах для n-го отсчета
         
