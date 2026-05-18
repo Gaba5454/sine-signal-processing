5b455044a841fd87e53f4ff3d2915508e728a2c0
Gaba5454 <egor.shvarev@bk.ru>
2026-05-18 16:15:32 +0700

    Rename functions

--- include/tests.cpp
+++ include/tests.cpp
@@ -1,18 +1,17 @@
 #include "tests.h"
 
 
-double TestQuantizationNoise(double freq, double fs, int num_samples) {
-    // 1. Генератор синуса, на частоте которая задается вручную
+double test_quantizationNoise(double freq, double fs, int num_samples) {
+
     std::vector<double> signal = sine_generator(freq, fs, num_samples);
 
-    // 2. Операция квантования и обработка ошибки квантования
     std::vector<int16_t> quant_sig = quantization(signal);
     double error = quantization_error(signal, quant_sig);
     std::cout << error << "\n";
     return error;
 }
 
-void WriteQuantizationResultToCsv(const std::string& filepath, double freq, double error) {
+void writeResults_quantizationt_toCsv(const std::string& filepath, double freq, double error) {
 
     std::ofstream file(filepath, std::ios::app);
     
@@ -32,22 +31,19 @@ void WriteQuantizationResultToCsv(const std::string& filepath, double freq, doub
 
 
 
-double TestFloatQualityOfInterpolation(double freq, double fs, int num_samples) {
+double test_qualityInterpolation_float(double freq, double fs, int num_samples) {
 
-    // 1. Генератор синуса, на частоте которая задается вручную
     std::vector<double> signal = sine_generator(freq, fs, num_samples);
     std::vector<double> signal_ideal = sine_generator(freq, fs * 2, num_samples * 2);
 
-    // 2. Функция интерполяции с плавающей точкой
     std::vector<double> inter_sig = linear_interpolation_float_point(signal);
     
-    // 3. Анализ интерполяции
     double error = interpolation_error_float_point(signal_ideal, inter_sig);
 
     return error;
 }
 
-void WriteResultsFloatInterpolationToCsv(const std::string& filepath, double freq, double fs, double error){
+void writeResults_floatInterpolation_toCsv(const std::string& filepath, double freq, double fs, double error){
 
     std::ofstream file(filepath, std::ios::app);
     
@@ -67,28 +63,23 @@ void WriteResultsFloatInterpolationToCsv(const std::string& filepath, double fre
 
 
 
-double TestIntQualityOfInterpolation(double freq, double fs, int num_samples) {
+double test_qualityIterpolation_fixed(double freq, double fs, int num_samples) {
 
-    // Генератор синуса
     std::vector<double> signal = sine_generator(freq, fs, num_samples);
     std::vector<double> signal_ideal = sine_generator(freq, fs * 2, num_samples * 2);
    
-    // Операция квантования для сигнала под интерполяцию
     std::vector<int16_t> quant_sig = quantization(signal);
 
-    // Операция квантования для эталонного сигнала
     std::vector<int16_t> quant_sig_ideal = quantization(signal_ideal);
 
-    // Функция интерполяции с фиксированной точкой
     std::vector<int16_t> inter_sig_quant = linear_interpolation_fixed_point(quant_sig);
 
-    // Анализ интерполяции
     double error = interpolation_error_fixed_point(quant_sig_ideal, inter_sig_quant);
     
     return error;
 }
 
-void WriteResultsIntInterpolationToCsv(const std::string& filepath, double freq, double fs, double error){
+void writeResults_fixedInterpolation_toCsv(const std::string& filepath, double freq, double fs, double error){
 
     std::ofstream file(filepath, std::ios::app);
     
@@ -106,4 +97,20 @@ void WriteResultsIntInterpolationToCsv(const std::string& filepath, double freq,
     file.close();
 }
 
-
+void writeResults_allInterpolation_toCsv(const std::string& filepath, double freq, double fs, double float_error, double fixed_error){
+    
+    std::ofstream file(filepath, std::ios::app);
+    
+    if (!file.is_open()) {
+        std::cerr << "Error: cannot open file " << filepath << "\n";
+        return;
+    }
+    
+    if (file.tellp() == 0) {
+        file << "freq_hz,fs,float_err,fixed_err,difference\n";  
+    }
+    double difference = std::abs(float_error - fixed_error);
+    file << freq << "," << fs << "," << float_error << "," << fixed_error << "," << difference <<"\n";
+    
+    file.close();
+}
--- include/tests.h
+++ include/tests.h
@@ -6,11 +6,13 @@
 #include "quantization.h"
 #include "interpolation.h"
 
-double TestQuantizationNoise(double freq, double fs, int num_samples);
-void   WriteQuantizationResultToCsv(const std::string& filepath, double freq, double error);
+double test_quantizationNoise(double freq, double fs, int num_samples);
+void   writeResults_quantizationt_toCsv(const std::string& filepath, double freq, double error);
 
-double TestFloatQualityOfInterpolation(double freq, double fs, int num_samples);
-void   WriteResultsFloatInterpolationToCsv(const std::string& filepath, double freq, double fs, double error);
+double test_qualityInterpolation_float(double freq, double fs, int num_samples);
+void   writeResults_floatInterpolation_toCsv(const std::string& filepath, double freq, double fs, double error);
 
-double TestIntQualityOfInterpolation(double freq, double fs, int num_samples);
-void   WriteResultsIntInterpolationToCsv(const std::string& filepath, double freq, double fs, double error);
+double test_qualityIterpolation_fixed(double freq, double fs, int num_samples);
+void   writeResults_fixedInterpolation_toCsv(const std::string& filepath, double freq, double fs, double error);
+
+void writeResults_allInterpolation_toCsv(const std::string& filepath, double freq, double fs, double float_error, double fixed_error);
