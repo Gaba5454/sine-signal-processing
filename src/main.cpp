5d57574ad43c143294738b4453cab7f88be5501e
Gaba5454 <egor.shvarev@bk.ru>
2026-05-18 10:59:12 +0700

    Quick fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,15 @@ int main() {
     const int num_samples = 50;
 
     for (double freq : {2.0, 10.0, 25.0, 33.0, 49.0}) {
-    std::string csv_path = "results/QuantizationNoiseData.csv";
+    std::string csv_path = "../results/QuantizationNoiseData.csv";
     double error = TestQuantizationNoise(freq, fs, num_samples);
     WriteQuantizationResultToCsv(csv_path, freq, error);
 
-    csv_path = "results/FloatQualityOfInterpolationData.csv";
+    csv_path = "../results/FloatQualityOfInterpolationData.csv";
     error = TestFloatQualityOfInterpolation(freq, fs, num_samples);
     WriteResultsFloatInterpolationToCsv(csv_path, freq, fs, error);    
 
-    csv_path = "results/IntQualityOfInterpolationData.csv";
+    csv_path = "../results/IntQualityOfInterpolationData.csv";
     error = TestIntQualityOfInterpolation(freq, fs, num_samples);
     WriteResultsIntInterpolationToCsv(csv_path, freq, fs, error);
     }
@@ -24,5 +24,9 @@ int main() {
 
 }
 
+// Написать ещё один метод интерполяции
 // Магическое число 32767
 // Сравнение float и int 
+// Создать CMake
+// Написать отчёт
+// Напиать readme
